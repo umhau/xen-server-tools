3bd02db7e67778b2e953cb76b84cc0d6b02b44f5
um.hau <um.hau@outlook.com>
2020-11-03 14:49:29 -0500

    typo

--- remove_raid_device.sh
+++ remove_raid_device.sh
@@ -3,7 +3,7 @@
 set -e
 set -v
 
-# usage: bash remove_raid_device.sh /dev/mdX 
+# usage: bash remove_raid_device.sh -m /dev/mdX -d "e v f m"
 
 while getopts ":m:d:" opt; do
   case $opt in
@@ -24,7 +24,7 @@ devicelist=$(eval echo  /dev/sd{`echo "$drive_list" | tr ' ' ,`}1)
 echo "Drives connected to RAID: $devicelist"
 echo -n "Removing RAID device $1. Confirm > "; read
 
-if mount | grep $1 > /dev/null; then unmount $1; fi   # unmount drive if mounted
+if mount | grep $1 > /dev/null; then umount $1; fi    # unmount drive if mounted
 
 mdadm --stop "$1"                                       # deactivate RAID device
 
